f9301384358524ef357acbbfbaa8b135aa3b3638
Jyoti Bhardwaj <jyotibhardwaj2798@gmail.com>
2026-02-15 17:55:38 +0530

    Update streamlit_app.py

--- Medical_Appointment_No-Show_Prediction_and_Demand_Forecasting/streamlit_app.py
+++ Medical_Appointment_No-Show_Prediction_and_Demand_Forecasting/streamlit_app.py
@@ -16,10 +16,10 @@ menu = st.sidebar.radio("Navigation",
 
 @st.cache_resource
 def load_all():
-    clf = joblib.load("Medical-Appointment-No-Show-Prediction-Demand-Forecasting/no_show_model.pkl")
-    enc = joblib.load("Medical-Appointment-No-Show-Prediction-Demand-Forecastingencoders.pkl")
-    demand = joblib.load("Medical-Appointment-No-Show-Prediction-Demand-Forecastingdemand_forecast_model_v2.pkl")
-    demand_features = joblib.load("Medical-Appointment-No-Show-Prediction-Demand-Forecastingdemand_features_v2.pkl")
+    clf = joblib.load("/mount/src/medical-appointment-no-show-prediction-demand-forecasting/no_show_model.pkl")
+    enc = joblib.load("/mount/src/medical-appointment-no-show-prediction-demand-forecasting/encoders.pkl")
+    demand = joblib.load("/mount/src/medical-appointment-no-show-prediction-demand-forecasting/demand_forecast_model_v2.pkl")
+    demand_features = joblib.load("/mount/src/medical-appointment-no-show-prediction-demand-forecasting/demand_features_v2.pkl")
     return clf, enc, demand, demand_features
 
 clf, encoders, demand_model, demand_features = load_all()
